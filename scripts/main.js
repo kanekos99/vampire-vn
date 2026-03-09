369c3ec51a51e2d98e60d937f34b735ce9c4a517
kanekos99 <pidgeons99@gmail.com>
2026-03-09 10:58:32 +0800

    Add preview gallery

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+const modalImg = document.getElementById("modal-image");
+
 const app = {
   init: function () {
     console.log("Hello world :)");
@@ -10,4 +12,29 @@ function loadGame() {
   document.getElementById("game-container").innerHTML =
     '<iframe frameborder="0" src="https://itch.io/embed-upload/16288679?color=000000" allowfullscreen="" width="1024" height="576"><a href="https://kanekos99.itch.io/how-not-to-kill-a-vampire-lord">Play How (Not) To Kill A Vampire Lord on itch.io</a></iframe>';
   document.getElementById("load-btn").style.display = "none";
+}
+
+function showNextOrPrevImg(direction) {
+  const visibleImages = [...document.querySelectorAll(".gallery-thumbnail")];
+  const currentSrc = modalImg.src;
+
+  let currentIndex = visibleImages.findIndex((img) => img.src === currentSrc);
+  let nextIndex = currentIndex + direction;
+
+  if (direction === 1 && nextIndex >= visibleImages.length) {
+    nextIndex = 0;
+  } else if (direction === -1 && nextIndex === -1) {
+    nextIndex = visibleImages.length - 1;
+  }
+
+  showImage(visibleImages[nextIndex]);
+}
+
+function showImage(image) {
+  modalImg.style.display = "none";
+  modalImg.src = image.src;
+
+  modalImg.onload = function () {
+    modalImg.style.display = "block";
+  };
 }
